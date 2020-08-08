f8dc1dbf4bd1a03cc78dd70396c6490ae0f35816
prajat <rajat.singh1084@gmail.com>
2020-08-09 00:17:58 +0530

    func

--- cards/main.go
+++ cards/main.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func main() {
 	//var card string = "Ace of spades"
-	card := "ace of spades"
-	card = "five of hearts"
+	card := newCard()
+	//card = "five of hearts"
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "four of hearts"
 }
