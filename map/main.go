c41beb9a13e563bd530bb50be473cbf08cf690cd
prajat <rajat.singh1084@gmail.com>
2020-08-31 12:38:28 +0530

    map operations

--- map/main.go
+++ map/main.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	//var colors map[string]string      just declaring using a var keyword.
+
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+	}
+
+	colors["white"] = "#ffffff"
+
+	//delete(colors, "white")
+
+	//fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("hex code for ", color, "is", hex)
+	}
 }
