5c2c03dbd3bbf587ff499d592c8a4461b2f1924b
prajat <rajat.singh1084@gmail.com>
2020-08-31 16:29:35 +0530

    interface bot

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,8 +1,40 @@
 package main
 
+import "fmt"
+
 type englishBot struct{}
 type spanishBot struct{}
 
+type bot interface {
+	getGreeting() string
+}
+
 func main() {
 
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+
 }
+
+func (englishBot) getGreeting() string {
+	//very custom language for english
+	return "hi there!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "hola!"
+}
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+// func printGretting(eb englishBot) {
+// 	fmt.Println(eb.getGreeting())
+// }                                                 //dont need to make 2 function of same logic instead weel use interface and make only one function
+
+// func printGretting(sb spanishBot) {
+// 	fmt.Println(sb.getGreeting())
+// }
